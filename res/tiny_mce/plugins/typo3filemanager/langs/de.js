6a9fc466af043981c4408640033e1746c13c2819
Thomas Allmer <thomas.allmer@webteam.at>
2008-11-21 22:04:07 +0000

    typo3filemanager as TinyMCE Plugin (currently not possible with patcher)

---
+++ res/tiny_mce/plugins/typo3filemanager/langs/de.js
@@ -0,0 +1,4 @@
+tinyMCE.addI18n('de.typo3filemanger',{
+	link_desc : 'TYPO3 Verkn\u00FCpfung',
+	image_desc : 'TYPO3 Bild'
+});
---
+++ res/tiny_mce/plugins/typo3filemanager/langs/en.js
@@ -0,0 +1,4 @@
+tinyMCE.addI18n('en.typo3filemanger',{
+	link_desc : 'TYPO3 Link',
+	image_desc : 'TYPO3 Image'
+});
