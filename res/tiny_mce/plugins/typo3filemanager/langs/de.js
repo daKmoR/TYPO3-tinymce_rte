87c164ed62c4bb83a34caa80d89837509baae685
Thomas Allmer <thomas.allmer@webteam.at>
2008-11-24 00:10:47 +0000

    remove typo3filemanager

--- res/tiny_mce/plugins/typo3filemanager/langs/de.js
+++
@@ -1,4 +0,0 @@
-tinyMCE.addI18n('de.typo3filemanager',{
-	link_desc : 'TYPO3 Verkn\u00FCpfung',
-	image_desc : 'TYPO3 Bild'
-});
--- res/tiny_mce/plugins/typo3filemanager/langs/en.js
+++
@@ -1,4 +0,0 @@
-tinyMCE.addI18n('en.typo3filemanager',{
-	link_desc : 'TYPO3 Link',
-	image_desc : 'TYPO3 Image'
-});
