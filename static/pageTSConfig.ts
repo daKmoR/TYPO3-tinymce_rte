ba2c918d04569fc8c42fc6f209fda331a0ac1d43
Thomas Allmer <thomas.allmer@webteam.at>
2008-12-15 21:20:44 +0000

    added config for FE usage

--- static/pageTSConfig.ts
+++ static/pageTSConfig.ts
@@ -2,3 +2,11 @@
 RTE.default {
 	loadConfig = EXT:tinymce_rte/static/standard.ts
 }
+
+RTE.default.FE >
+RTE.default.FE {
+	loadConfig = EXT:tinymce_rte/static/minimal.ts
+	init {
+		theme_advanced_resize_horizontal = false
+	}
+}
