ef3782bc024453b38ecb79e50f70d69af045d017
Thomas Allmer <daKmoR@gmx.at>
2009-12-03 10:18:34 +0100

    ok it works with TYPO3 4.3.

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
-			'typo3' => '4.2.0-4.2.99',
+			'typo3' => '4.2.0-4.3.99',
 		),
 		'conflicts' => array(
 			'ch_rterecords' => '',
