afc7ac6b7207c0b50855b616db172d513301b80d
daKmoR <daKmoR@gmx.at>
2010-09-29 14:57:03 +0200

    #9387 add dl, dd, dt, addresse to allowed Tags in page and setup TS

--- static/setupTSConfig.ts
+++ static/setupTSConfig.ts
@@ -1,13 +1,13 @@
 lib.parseFunc_RTE {
 	
 	# COPY ALLOW TAGS FROM PAGETS (WITHOUT b, i, link) sometimes there is a probblem with multiline so just use one line
-	allowTags =	a, abbr, acronym, address, blockquote, br, caption, cite, code, div, em, embed, h1, h2, h3, h4, h5, h6, hr, img, li, object, ol, p, param, pre, q, span, strong, sub, sup, table, tbody, td, th, tr, tt, ul
+	allowTags =	a, abbr, acronym, address, blockquote, br, caption, cite, code, dd, div, dl, dt, em, embed, h1, h2, h3, h4, h5, h6, hr, img, li, object, ol, p, param, pre, q, span, strong, sub, sup, table, tbody, td, th, tr, tt, ul
 	
 	# DO NOT ADD class="bodytext" TO EACH LINE
 	nonTypoTagStdWrap.encapsLines.addAttributes.P.class >
 
 	# DO NOT WRAP THOSE TAGS WITH <p>
-	nonTypoTagStdWrap.encapsLines.encapsTagList = cite, div, p, pre, hr, h1, h2, h3, h4, h5, h6, table
+	nonTypoTagStdWrap.encapsLines.encapsTagList = address, cite, div, dl, hr, h1, h2, h3, h4, h5, h6, p, pre, table
 	
 	# table handling
 	table.stripNL = 1
